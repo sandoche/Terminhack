5454bbba0a4851398552563c82d2cdf0ca2e892a
Sandoche Adittane <sandoche@protonmail.com>
2021-02-28 10:26:54 +0000

    :construction: Removed index and replaced by default hack controller

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var indexController = require('./controller/indexController')
-var hackController = require('./controller/hackController')
+const hackController = require('./controller/hackController')
 
-var app = express()
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -19,8 +18,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexController)
-app.use('/:hack', hackController)
+app.use('/:hack?/:target?', hackController)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
