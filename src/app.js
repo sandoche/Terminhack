437ceb31e544679927954dc491aec059ef0c992a
Sandoche Adittane <sandoche@protonmail.com>
2021-02-28 09:56:26 +0000

    :sparkles: Updating code structure, closes #4

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexController = require('./controller/indexController');
+var hackController = require('./controller/hackController');
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexController);
+app.use('/:hack', hackController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ src/controller/hackController.js
@@ -0,0 +1,24 @@
+import express from "express";
+const router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.setHeader("Transfer-Encoding", "chunked");
+  res.write("Thinking...");
+  sendAndSleep(res, 1);
+});
+
+var sendAndSleep = function (response, counter) {
+  if (counter > 10) {
+    response.end();
+  } else {
+    response.write(`;i=` + counter);
+    counter++;
+    setTimeout(function () {
+      sendAndSleep(response, counter);
+    }, 1000);
+  }
+};
+
+export default router;
