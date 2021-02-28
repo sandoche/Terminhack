1dba2cedb5082b552663f363116e281883cd9110
Sandoche Adittane <sandoche@protonmail.com>
2021-02-28 11:38:02 +0000

    :sparkles: Parsing of yml implemented

--- src/service/hackDataParsingService/index.js
+++ src/service/hackDataParsingService/index.js
@@ -1,9 +1,29 @@
+import yaml from 'js-yaml'
+import fs from 'fs'
+import path from 'path'
 
 const parse = (hackFile, targetHost) => {
-  // import data
-  // replace targetHost everywhere
+  const hackDataRaw = importData(hackFile)
+  const hackDataWithContent = replaceTargetHost(hackDataRaw, targetHost)
 
-  return {}
+  return hackDataWithContent
+}
+
+const importData = (hackFile) => {
+  const data = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../data/' + hackFile), 'utf8'))
+  return data
+}
+
+const replaceTargetHost = (data, targetHost) => {
+  const dataWithContent = data.map((item) => {
+    try {
+      item.value = item.value.replace(/\$target/g, targetHost)
+    } catch (e) {}
+
+    return item
+  })
+
+  return dataWithContent
 }
 
 export default {
